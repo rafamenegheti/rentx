283e78c00ed7087c7839dd5d872b272990110ff2
Rafael Menegheti <rafaelmenegheti52@gmail.com>
2022-01-20 08:57:09 -0300

    Delete dist directory

--- dist/shared/container/Providers/DateProvider/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _tsyringe = require("tsyringe");
-
-var _DayJsDateProvider = require("./implementations/DayJsDateProvider");
-
-_tsyringe.container.registerSingleton("DateProvider", _DayJsDateProvider.DayJsDateProvider);
